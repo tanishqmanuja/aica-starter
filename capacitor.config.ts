440ec45d9f980bff5f6ebef0fd6ea8fccbaa4551
tanishqmanuja <tanishqmanuja@gmail.com>
2022-11-29 13:30:24 +0530

    feat(capacitor): add android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'aica.starter.io',
+  appName: 'aica-starter',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

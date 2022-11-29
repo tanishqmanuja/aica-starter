19eca53fecb0da9dbdec7521b345b07fbdaa43f6
tanishqmanuja <tanishqmanuja@gmail.com>
2022-11-29 17:45:05 +0530

    fix(android): remove flicker after splash screen
    # For android SDK >= 31 change statusbar and webview color to splash bg color

--- android/app/src/main/java/aica/starter/io/MainActivity.java
+++ android/app/src/main/java/aica/starter/io/MainActivity.java
@@ -1,5 +1,19 @@
 package aica.starter.io;
 
+import android.os.Build;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+      int color = getColor(R.color.splashBackground);
+      this.bridge.getWebView().setBackgroundColor(color);
+      getWindow().setStatusBarColor(color);
+    }
+  }
+}

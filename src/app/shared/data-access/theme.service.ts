0b656bce39992b0e019e7d8c7334401a2014a51c
tanishqmanuja <tanishqmanuja@gmail.com>
2022-12-02 14:13:21 +0530

    feat: auto dark mode using rxjs

--- src/app/shared/data-access/theme.service.ts
+++ src/app/shared/data-access/theme.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
 import { StatusBar } from '@capacitor/status-bar';
 import { MonetController } from '@tanishqmanuja/capacitor-plugin-monet';
+import { tap } from 'rxjs';
+import { isDarkMode } from '../util/prefers-color-scheme';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +15,15 @@ export class ThemeService {
   init() {
     SafeAreaController.injectCSSVariables();
     MonetController.autoInjectStyle();
+
+    isDarkMode()
+      .pipe(
+        tap((value) => {
+          document.body.classList.remove(value ? 'light' : 'dark');
+          document.body.classList.add(value ? 'dark' : 'light');
+        })
+      )
+      .subscribe();
 
     if (Capacitor.getPlatform() === 'android') {
       StatusBar.setOverlaysWebView({ overlay: true });

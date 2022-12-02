dda684255d00d21c0acb404f61cc4192e9c4b56e
tanishqmanuja <tanishqmanuja@gmail.com>
2022-12-02 13:19:19 +0530

    feat: add monet to theme service

--- src/app/shared/data-access/theme.service.ts
+++ src/app/shared/data-access/theme.service.ts
@@ -2,6 +2,7 @@ import { SafeAreaController } from '@aashu-dubey/capacitor-statusbar-safe-area';
 import { Injectable } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
 import { StatusBar } from '@capacitor/status-bar';
+import { MonetController } from '@tanishqmanuja/capacitor-plugin-monet';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +12,7 @@ export class ThemeService {
 
   init() {
     SafeAreaController.injectCSSVariables();
+    MonetController.autoInjectStyle();
 
     if (Capacitor.getPlatform() === 'android') {
       StatusBar.setOverlaysWebView({ overlay: true });

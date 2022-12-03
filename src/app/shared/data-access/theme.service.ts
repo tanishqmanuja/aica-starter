76229f583db45b13e9fd77ce27f5132abb015dad
tanishqmanuja <tanishqmanuja@gmail.com>
2022-12-03 15:32:21 +0530

    feat: change statusbar style on theme change

--- src/app/shared/data-access/theme.service.ts
+++ src/app/shared/data-access/theme.service.ts
@@ -1,7 +1,7 @@
 import { SafeAreaController } from '@aashu-dubey/capacitor-statusbar-safe-area';
 import { Injectable } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
-import { StatusBar } from '@capacitor/status-bar';
+import { StatusBar, Style } from '@capacitor/status-bar';
 import { MonetController } from '@tanishqmanuja/capacitor-plugin-monet';
 import { tap } from 'rxjs';
 import { isDarkMode } from '../util/prefers-color-scheme';
@@ -21,6 +21,10 @@ export class ThemeService {
         tap((value) => {
           document.body.classList.remove(value ? 'light' : 'dark');
           document.body.classList.add(value ? 'dark' : 'light');
+
+          if (Capacitor.getPlatform() === 'android') {
+            StatusBar.setStyle({ style: value ? Style.Dark : Style.Light });
+          }
         })
       )
       .subscribe();

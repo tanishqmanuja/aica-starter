9b106e8cd99ae2bd4a58a892ac9e2d4764b9607a
tanishqmanuja <tanishqmanuja@gmail.com>
2022-12-03 16:45:28 +0530

    fix(capacitor): add plugin for darkmode
    # because of issue https://github.com/ionic-team/capacitor-plugins/issues/1010

--- src/app/shared/data-access/theme.service.ts
+++ src/app/shared/data-access/theme.service.ts
@@ -1,11 +1,9 @@
 import { SafeAreaController } from '@aashu-dubey/capacitor-statusbar-safe-area';
 import { Injectable } from '@angular/core';
+import { DarkMode } from '@aparajita/capacitor-dark-mode';
 import { Capacitor } from '@capacitor/core';
-import { StatusBar, Style } from '@capacitor/status-bar';
+import { StatusBar } from '@capacitor/status-bar';
 import { MonetController } from '@tanishqmanuja/capacitor-plugin-monet';
-import { tap } from 'rxjs';
-import { isDarkMode } from '../util/prefers-color-scheme';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -16,18 +14,9 @@ export class ThemeService {
     SafeAreaController.injectCSSVariables();
     MonetController.autoInjectStyle();
 
-    isDarkMode()
-      .pipe(
-        tap((value) => {
-          document.body.classList.remove(value ? 'light' : 'dark');
-          document.body.classList.add(value ? 'dark' : 'light');
-
-          if (Capacitor.getPlatform() === 'android') {
-            StatusBar.setStyle({ style: value ? Style.Dark : Style.Light });
-          }
-        })
-      )
-      .subscribe();
+    DarkMode.configure({
+      syncStatusBar: true,
+    });
 
     if (Capacitor.getPlatform() === 'android') {
       StatusBar.setOverlaysWebView({ overlay: true });

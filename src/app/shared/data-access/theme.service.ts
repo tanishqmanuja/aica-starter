97af3ec4b9b8a45a1a088a62d3025af2370644aa
tanishqmanuja <tanishqmanuja@gmail.com>
2023-01-05 12:36:12 +0530

    fix: status-bar theme

--- src/app/shared/data-access/theme.service.ts
+++ src/app/shared/data-access/theme.service.ts
@@ -2,8 +2,10 @@ import { SafeAreaController } from '@aashu-dubey/capacitor-statusbar-safe-area';
 import { Injectable } from '@angular/core';
 import { DarkMode } from '@aparajita/capacitor-dark-mode';
 import { Capacitor } from '@capacitor/core';
-import { StatusBar } from '@capacitor/status-bar';
+import { StatusBar, Style } from '@capacitor/status-bar';
 import { MonetController } from '@tanishqmanuja/capacitor-plugin-monet';
+import { take, tap } from 'rxjs';
+import { isDarkMode } from '../util/prefers-color-scheme';
 @Injectable({
   providedIn: 'root',
 })
@@ -17,6 +19,17 @@ export class ThemeService {
     DarkMode.configure({
       syncStatusBar: true,
     });
+
+    isDarkMode()
+      .pipe(
+        take(1),
+        tap((value) => {
+          if (Capacitor.getPlatform() === 'android') {
+            StatusBar.setStyle({ style: value ? Style.Dark : Style.Light });
+          }
+        })
+      )
+      .subscribe();
 
     if (Capacitor.getPlatform() === 'android') {
       StatusBar.setOverlaysWebView({ overlay: true });
